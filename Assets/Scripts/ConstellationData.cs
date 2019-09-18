c36f66985893dcccdf80763dc62ecd6600d8813e
Kengo Baba <kengo0316.giants@gmail.com>
2019-09-18 14:26:08 +0900

    scripts

---
+++ Assets/Scripts/ConstellationData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+public class ConstellationData
+{
+    public ConstellationNameData Name;            // 星座名のデータ
+    public ConstellationPositionData Position;    // 星座位置
+    public List<StarData> Stars;                      // 星の位置
+    public List<ConstellationLineData> Lines;         // 星座線のデータ
+}
---
+++ Assets/Scripts/VisibleConstellationLine.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class VisibleConstellationLine : MonoBehaviour
+{
+    GameObject lines;            // Linesゲームオブジェクト
+
+    void Start()
+    {
+        // 親からLinesを検索する
+        var consstellation = transform.GetComponentInParent<DrawConstellation>();
+        lines = consstellation.LinesParent;
+        // 星座線を非表示にする
+        lines.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // レイヤーがViewHitかどうか
+        if (other.gameObject.layer == LayerMask.NameToLayer("ViewHit"))
+        {
+            // コライダーに当たらなくなったら非表示にする
+            lines.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        // レイヤーがViewObjectかどうか
+        if (other.gameObject.layer == LayerMask.NameToLayer("ViewHit"))
+        {
+            // コライダーに当たらなくなったら非表示にする
+            lines.SetActive(false);
+        }
+    }
+}
